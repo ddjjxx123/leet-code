a726aedb05a86efb18a1b2fd80e826947de4c256
alex <254318118@qq.com>
2021-07-01 17:52:55 +0800

    lcp seven

--- src/com/alexhades/leetCode/lcp/LCPSeven.java
+++ src/com/alexhades/leetCode/lcp/LCPSeven.java
@@ -2,6 +2,8 @@ package com.alexhades.leetCode.lcp;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class LCPSeven {
     @Test
     public void fn() {
@@ -9,11 +11,40 @@ public class LCPSeven {
                 new int[][]{{0, 2}, {2, 1}, {3, 4}, {2, 3}, {1, 4}, {2, 0}, {0, 4}},
                 3));
     }
+    public int numWaysDp(int n, int[][] relation, int k) {
+        //经过 I轮 到达J 的有几种方式
+        int[][] dp=new int[k+1][n];
+//        dp[0][0]=1;
+        dp[0][0]=1;
+        for (int i = 0; i < k; i++) {
+            for (int[] real:relation) {
+                int src=real[0];
+                int dst=real[1];
+                dp[i+1][dst]+=dp[i][src];
+            }
+        }
+        return dp[k][n-1];
+    }
+
 
     public int numWays(int n, int[][] relation, int k) {
-        int res=0;
-
-        return res;
-
+        //经过 I轮 到达J 的有几种方式
+//        int[][] dp=new int[k+1][n];
+        int[] dp=new int[n];
+//        dp[0][0]=1;
+        dp[0]=1;
+        for (int i = 0; i < k; i++) {
+            int[] temp=new int[n];
+            for (int[] real:relation) {
+                int src=real[0];
+                int dst=real[1];
+                temp[dst]+=dp[src];
+            }
+            dp=temp;
+        }
+        return dp[n-1];
     }
+
+
+
 }
